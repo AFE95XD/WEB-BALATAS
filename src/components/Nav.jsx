080870d063195de3a48d93f5870dafbc993706e9
AFE <afe95xd@gmail.com>
2022-12-22 00:25:43 -0600

    modificando nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -11,11 +11,12 @@ const Nav = () => {
       <div className="flex h-10 items-center justify-center bg-indigo-600 px-4 text-sm font-medium text-white sm:px-6 lg:px-8">
         Llámanos: ​​(722) 238 5244, (722) 705 9029, (722) 471 1254
       </div>
-      <Disclosure as="nav" className="bg-gray-800 fixed z-40 w-full">
+      <Disclosure as="nav" className="bg-white fixed z-40 w-full">
         {({ open }) => (
           <>
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
               <div className="flex h-16 items-center justify-between">
+                {/* ESTE ES EL DIN DEL LOGO */}
                 <div className="flex-shrink-0">
                   <img
                     className="block h-8 w-auto lg:hidden"
@@ -28,36 +29,50 @@ const Nav = () => {
                     alt="Your Company"
                   />
                 </div>
+                {/* DIV DEL MENU */}
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
                     {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
                     <a
                       href="#"
-                      className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
+                      className="rounded-md px-3 py-2 text-sm font-medium text-gray-900 hover:bg-[#db2d2e] hover:text-white"
                     >
                       Dashboard
                     </a>
                     <a
                       href="#"
-                      className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                      className="rounded-md px-3 py-2 text-sm font-medium text-gray-900 hover:bg-[#db2d2e] hover:text-white"
                     >
                       Team
                     </a>
                     <a
                       href="#"
-                      className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                      className="rounded-md px-3 py-2 text-sm font-medium text-gray-900 hover:bg-[#db2d2e] hover:text-white"
                     >
                       Projects
                     </a>
                     <a
                       href="#"
-                      className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                      className="rounded-md px-3 py-2 text-sm font-medium text-gray-900 hover:bg-[#db2d2e] hover:text-white"
                     >
                       Calendar
                     </a>
                   </div>
                 </div>
-                {/* <div className="flex items-center justify-between"></div> */}
+                <div className="hidden sm:ml-6 sm:block">
+                  <a
+                    href="#"
+                    className="rounded-md px-3 py-2 text-sm font-medium text-gray-900 hover:bg-gray-900 hover:text-white"
+                  >
+                    Regístrate
+                  </a>
+                  <a
+                    href="#"
+                    className="rounded-md px-3 py-2 text-sm font-medium text-white bg-[#db2d2e] hover:bg-gray-900 hover:text-white"
+                  >
+                    Iniciar Sesión
+                  </a>
+                </div>
 
                 <div className="-mr-2 flex sm:hidden">
                   {/* Mobile menu button */}
