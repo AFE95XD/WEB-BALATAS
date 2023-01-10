27057a3c10ea855f918dfef633757b9d599fc4c5
AFE <afe95xd@gmail.com>
2023-01-10 00:28:33 -0600

    baner servicios responsive arreglado

--- src/components/Servicios/SecServiciosBaner.jsx
+++ src/components/Servicios/SecServiciosBaner.jsx
@@ -22,40 +22,101 @@ const contenido = [
 
 const SecServiciosBaner = () => {
   return (
-    <section className="bg-[#FF9B16]">
-      <div className="grid sm:grid-cols-1 sm:gap-x-px md:grid-cols-3 mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8 justify-items-center">
-        {contenido.map((item, i) => (
-          <div
-            className="flex justify-items-center content-center flex-wrap"
-            key={i}
-          >
-            <item.icono className="w-20 h-20 text-white pr-2" />
-            <div className="w-80">
+    <section className="w-full bg-[#FF9B16]">
+      <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8">
+        {/* ESTE ES GRID */}
+        {/* <div className="grid grid-cols-1 gap-y-20 md:grid-cols-3 md:gap-y-8 lg:gap-x-8"> */}
+        {/* ESTE ES FLEX */}
+        <div className="flex md:flex-row md:gap-y-10 flex-col gap-10">
+          {/* <div className="flex flex-col items-center lg:flex-row">
+            <MapPinIcon className="w-20 h-20 text-white pr-2" />
+            <div className="w-full text-center">
               <h3 className="font-roboto text-white font-bold text-xl pb-2">
-                {item.titulo}
+                Dirección
               </h3>
               <p className="font-openSans text-white font-normal text-base">
-                {item.descripcion}
+                Av. Solidaridad Las Torres 1543 San Salvador Tizatlalli Metepec,
+                Edo. Méx., C.P. 52172
               </p>
             </div>
           </div>
-        ))}
-        {/* <div>
-          <div className="">
-            <h3 className="font-roboto ">Telefonos</h3>
-            <p className="font-openSans">
-              (722) 238 5244, (722) 705 9029, (722) 471 1254
-            </p>
+
+          <div className="flex flex-col items-center lg:flex-row">
+            <MapPinIcon className="w-20 h-20 text-white pr-2" />
+            <div className="w-full text-center">
+              <h3 className="font-roboto text-white font-bold text-xl pb-2">
+                Dirección
+              </h3>
+              <p className="font-openSans text-white font-normal text-base">
+                Av. Solidaridad Las Torres 1543 San Salvador Tizatlalli Metepec,
+                Edo. Méx., C.P. 52172
+              </p>
+            </div>
           </div>
+
+          <div className="flex flex-col items-center lg:flex-row">
+            <MapPinIcon className="w-20 h-20 text-white pr-2" />
+            <div className="w-full text-center">
+              <h3 className="font-roboto text-white font-bold text-xl pb-2">
+                Dirección
+              </h3>
+              <p className="font-openSans text-white font-normal text-base">
+                Av. Solidaridad Las Torres 1543 San Salvador Tizatlalli Metepec,
+                Edo. Méx., C.P. 52172
+              </p>
+            </div>
+          </div> */}
+          {/* -------Este es para FLEX------- */}
+          {/* <div className="flex items-center flex-col lg:flex-row">
+            <MapPinIcon className="w-20 h-20 text-white pr-2" />
+            <div className="w-full text-center">
+              <h3 className="font-roboto text-white font-bold text-xl pb-2">
+                Dirección
+              </h3>
+              <p className="font-openSans text-white font-normal text-base">
+                Av. Solidaridad Las Torres 1543 San Salvador Tizatlalli Metepec,
+                Edo. Méx., C.P. 52172
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center flex-col lg:flex-row">
+            <MapPinIcon className="w-20 h-20 text-white pr-2" />
+            <div className="w-full text-center">
+              <h3 className="font-roboto text-white font-bold text-xl pb-2">
+                Dirección
+              </h3>
+              <p className="font-openSans text-white font-normal text-base">
+                Av. Solidaridad Las Torres 1543 San Salvador Tizatlalli Metepec,
+                Edo. Méx., C.P. 52172
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center flex-col lg:flex-row">
+            <MapPinIcon className="w-20 h-20 text-white pr-2" />
+            <div className="w-full text-center">
+              <h3 className="font-roboto text-white font-bold text-xl pb-2">
+                Dirección
+              </h3>
+              <p className="font-openSans text-white font-normal text-base">
+                Av. Solidaridad Las Torres 1543 San Salvador Tizatlalli Metepec,
+                Edo. Méx., C.P. 52172
+              </p>
+            </div>
+          </div> */}
+          {contenido.map((item, i) => (
+            <div className="flex items-center flex-col lg:flex-row" key={i}>
+              <item.icono className="w-20 h-20 text-white pr-2" />
+              <div className="w-full text-center">
+                <h3 className="font-roboto text-white font-bold text-xl pb-2">
+                  {item.titulo}
+                </h3>
+                <p className="font-openSans text-white font-normal text-base">
+                  {item.descripcion}
+                </p>
+              </div>
+            </div>
+          ))}
         </div>
-        <div>
-          <div className="">
-            <h3 className="font-roboto ">Horarios</h3>
-            <p className="font-openSans">
-              LUNES - VIERNES: 9:00 AM - 6:00 PM ​SÁBADO: 9:00 AM - 2:00 PM
-            </p>
-          </div>
-        </div> */}
       </div>
     </section>
   );
